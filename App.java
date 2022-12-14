a7382a2cfaf8269c350ed1e633044da5e32b104d
lsalug00 <lsalug00@estudiantes.unileon.es>
2022-12-14 22:18:02 +0100

    Cambios en App.java y README.md

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Lidia Saludes Gonzalez");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("15/12/22");
+    System.out.println("Leon");
   }
   
 }
